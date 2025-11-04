b236492f9c30777b87c09dba4571fe39ea43ef82
kalsowerus <kalsowerus@protonmail.ch>
2025-11-04 11:33:58 +0100

    Reproduce video deletion bug

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 module.exports = {
+  video: true,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+        on('after:spec', (spec, results) => {
+            if (results && results.video) {
+                // Do we have failures for any retry attempts?
+                const failures = results.tests.some((test) =>
+                    test.attempts.some((attempt) => attempt.state === 'failed')
+                )
+                if (!failures) {
+                    // delete the video if the spec passed and no tests retried
+                    fs.unlinkSync(results.video)
+                }
+            }
+        })
+    },
   },
 }
